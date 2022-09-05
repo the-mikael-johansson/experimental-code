d5aeb879c384cd87a6e081a210d064556c893742
Mikael Johansson <the.mikael.johansson@gmail.com>
2022-09-05 08:02:52 +0200

    feat: callback test (restresult / restresponse)

---
+++ csharp-callbacktest/CallbackTest/CallbackTest/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Web.Mvc;
+using Mimiware.ServiceResult;
+using static System.Console;
+
+namespace CallbackTest;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var controller = new MyController();
+        var x = controller.GetUsers();
+        WriteLine($"x={x.GetType().FullName}");
+    }
+
+    public class MyController
+    {
+        private readonly TestService _service;
+
+        public MyController()
+        {
+            _service = new TestService();
+        }
+
+        public ActionResult GetUsers()
+        {
+            return GetResponse(() => _service.GetUsers());
+        }
+
+        private static ActionResult GetResponse<T>(Func<IServiceResult<T>> function)
+        {
+            // Check if valid Model
+
+            // Call method
+            var serviceResult = function();
+
+            // Return rest response
+            return RestResponse(serviceResult);
+        }
+    }
+
+    public interface ITestService
+    {
+        IServiceResult<string> GetUsers();
+    }
+
+    public class TestService : ITestService
+    {
+        public IServiceResult<string> GetUsers()
+        {
+            var result = new ServiceResult<string>();
+            return result.Ok("OK");
+        }
+    }
+
+
+
+    static HttpStatusCodeResult RestResponse(IServiceResult result)
+    {
+        return result.IsSuccessCode
+            ? new HttpStatusCodeResult(result.Code)
+            : new HttpStatusCodeResult(result.Code, result.ErrorMessage.ErrorMessage);
+    }
+
+    static ActionResult RestResponse<T>(IServiceResult<T> result, JsonRequestBehavior requestBehavior = JsonRequestBehavior.DenyGet)
+    {
+        if (!result.IsSuccessCode)
+        {
+            return new HttpStatusCodeResult(result.Code, result.ErrorMessage.ErrorMessage);
+        }
+
+        return Json(result.Data, requestBehavior);
+    }
+
+    // Mock
+    static ActionResult Json(object a, object b)
+    {
+        return new A();
+    }
+
+    public class A : ActionResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            
+        }
+    }
+}
